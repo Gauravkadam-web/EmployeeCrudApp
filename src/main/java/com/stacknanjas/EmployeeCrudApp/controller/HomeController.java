5ee516f4361489cd3750c35ab79679ba37f2e3f1
GAURAVKADAM007 <gkadam3847@gmail.com>
2026-07-21 21:10:14 +0530

    Adding an home controller

---
+++ src/main/java/com/stacknanjas/EmployeeCrudApp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.stacknanjas.EmployeeCrudApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String redirectToEmployees() {
+        return "redirect:/employees";
+    }
+}
